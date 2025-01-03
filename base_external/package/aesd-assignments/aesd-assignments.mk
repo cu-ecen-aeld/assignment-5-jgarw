31c85c32164acd28aefd6529b0ee6275fa393319
Joseph Garwood <joegarwood@proton.me>
2025-01-03 09:54:53 -0500

    update mk file

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = master
+AESD_ASSIGNMENTS_VERSION = d351a6117f371e6ec154af60875d309f4f36ba00
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
